039361161e3f281eebbb7d24909eda1ba27f980e
rcseven <rcedquila@gmail.com>
2023-06-28 02:38:35 +0800

    Made a display function

--- src/main.c
+++ src/main.c
@@ -2,7 +2,6 @@
 #define BTN_RST_14 P3_2
 #define BTN_RST_24 P3_3
 
-unsigned int game_time = 600;
 unsigned char game_minutes = 10;
 unsigned char game_seconds = 0;
 unsigned char shotclock_seconds = 24;
@@ -12,14 +11,78 @@ const int seven_segment[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66,
 void delay(unsigned int i)
 {
 
-    for (int j = i; j < 50; j++)
+    for (int j = i; j < 30; j++)
     {
-        for (int k = 0; k < 48; k++)
+        for (int k = 0; k < 30; k++)
         {
             __asm 
                 nop
+                nop
+                nop
+                nop
+                nop
             __endasm;
         }
+    }
+}
+
+void display(unsigned char i, unsigned char delay_time, unsigned char game_minutes, unsigned char game_seconds)
+{
+    for (int j = 0; j < 8; j++)
+    {
+
+        if (game_seconds < 24 && game_minutes == 0)
+        {
+            i = game_seconds;
+        }
+
+        // First Iter
+        P2 = seven_segment[i / 10];
+        P1 = seven_segment[game_minutes / 10];
+        P0_0 = 0;
+        P0_1 = 1;
+        P0_2 = 1;
+        P0_3 = 1;
+
+        P3_0 = 0;
+        P3_1 = 1;
+        delay(delay_time);
+
+        // Second Iter
+        P2 = seven_segment[i % 10];
+        P1 = seven_segment[game_minutes % 10];
+        P0_0 = 1;
+        P0_1 = 0;
+        P0_2 = 1;
+        P0_3 = 1;
+
+        P3_0 = 1;
+        P3_1 = 0;
+        delay(delay_time);
+
+        // Third Iter
+        P2 = seven_segment[i / 10];
+        P1 = seven_segment[game_seconds / 10];
+        P0_0 = 1;
+        P0_1 = 1;
+        P0_2 = 0;
+        P0_3 = 1;
+
+        P3_0 = 0;
+        P3_1 = 1;
+        delay(delay_time);
+
+        // Fourth Iter
+        P2 = seven_segment[i % 10];
+        P1 = seven_segment[game_seconds % 10];
+        P0_0 = 1;
+        P0_1 = 1;
+        P0_2 = 1;
+        P0_3 = 0;
+
+        P3_0 = 1;
+        P3_1 = 0;
+        delay(delay_time);
     }
 }
 
@@ -30,55 +93,12 @@ void main(void)
         for (int i = shotclock_seconds; i >= 0; i--)
         {
             P3_5 = 0;
-            for (int j = 0; j < 8; j++)
+
+            display(i, 1, game_minutes, game_seconds);
+        
+            if (game_seconds == 0 && game_minutes == 0)
             {
-                // First Iter
-                P2 = seven_segment[i / 10];
-                P1 = seven_segment[game_minutes / 10];
-                P0_0 = 0;
-                P0_1 = 1;
-                P0_2 = 1;
-                P0_3 = 1;
-
-                P3_0 = 0;
-                P3_1 = 1;
-                delay(1.8);
-
-                // Second Iter
-                P2 = seven_segment[i % 10];
-                P1 = seven_segment[game_minutes % 10];
-                P0_0 = 1;
-                P0_1 = 0;
-                P0_2 = 1;
-                P0_3 = 1;
-
-                P3_0 = 1;
-                P3_1 = 0;
-                delay(1.8);
-
-                // Third Iter
-                P2 = seven_segment[i / 10];
-                P1 = seven_segment[game_seconds / 10];
-                P0_0 = 1;
-                P0_1 = 1;
-                P0_2 = 0;
-                P0_3 = 1;
-
-                P3_0 = 0;
-                P3_1 = 1;
-                delay(1.8);
-
-                // Fourth Iter
-                P2 = seven_segment[i % 10];
-                P1 = seven_segment[game_seconds % 10];
-                P0_0 = 1;
-                P0_1 = 1;
-                P0_2 = 1;
-                P0_3 = 0;
-
-                P3_0 = 1;
-                P3_1 = 0;
-                delay(1.8);
+                break;
             }
 
             if (!BTN_RST_24)
@@ -91,24 +111,22 @@ void main(void)
                 shotclock_seconds = 14;
                 break;
             }
-            if (game_time == 0)
-            {
-                break;
-            }
-            
 
             if (game_seconds == 0)
             {
                 game_minutes--;
                 game_seconds = 59;
-            }
-            else
-            {
+            }else{
                 game_seconds--;
             }
-            game_time--;
         }
 
+        if (game_minutes == 0 && game_seconds == 0 && !BTN_RST_24)
+        {
+            game_minutes = 10;
+            game_seconds = 0;
+        }
+        
         while (BTN_RST_14 && BTN_RST_24)
         {
             P3_5 = 1;
